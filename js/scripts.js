01178f0b9c397c64412219ab03aaa9fa93248d0b
Krista Rutz and Sasa Schwartz <krista@>
2020-01-16 13:22:56 -0800

    prevent validation duplication

--- js/scripts.js
+++ js/scripts.js
@@ -20,15 +20,33 @@ $(document).ready(function() {
     var icecream = parseInt($("input:radio[name=icecream]:checked").val());
     var ridetype = parseInt($("input:radio[name=ridetype]:checked").val());
 
+    $("div.starbucks").removeClass("has-error");
+    $("div.plans").removeClass("has-error");
+    $("div.icecream").removeClass("has-error");
+    $("div.ridetype").removeClass("has-error");
+    $(".errorMessage").remove();
+
     if (starbucks && plans && icecream && ridetype) {
+
+      $("div.starbucks").removeClass("has-success");
+      $("div.plans").removeClass("has-success");
+      $("div.icecream").removeClass("has-success");
+      $("div.ridetype").removeClass("has-success");
+
       var resultNum = Math.round((starbucks + plans + icecream + ridetype) / 4) ;
       
       if (resultNum === 1) {
         $(".carousel").show();
+        $(".waterslide").hide();
+        $(".rollercoaster").hide();
       } else if (resultNum === 2) {
         $(".waterslide").show();
+        $(".carousel").hide();
+        $(".rollercoaster").hide();
       } else if (resultNum === 3) {
         $(".rollercoaster").show();
+        $(".waterslide").hide();
+        $(".carousel").hide();
       }
 
     } else {
@@ -36,25 +54,25 @@ $(document).ready(function() {
           $("div.starbucks").addClass("has-success");
         } else {
           $("div.starbucks").addClass("has-error");
-          $("div.starbucks").append('<label class="radio">Please select an option</label>');
+          $("div.starbucks").append('<label class="radio errorMessage">Please select an option</label>');
         }
         if (plans) {
           $("div.plans").addClass("has-success");
         } else {
           $("div.plans").addClass("has-error");
-          $("div.plans").append('<label class="radio">Please select an option</label>');
+          $("div.plans").append('<label class="radio errorMessage">Please select an option</label>');
         }
         if (icecream) {
           $("div.icecream").addClass("has-success");
         } else {
           $("div.icecream").addClass("has-error");
-          $("div.icecream").append('<label class="radio">Please select an option</label>');
+          $("div.icecream").append('<label class="radio errorMessage">Please select an option</label>');
         }
         if (ridetype) {
           $("div.ridetype").addClass("has-success");
         } else {
           $("div.ridetype").addClass("has-error");
-          $("div.ridetype").append('<label class="radio">Please select an option</label>');
+          $("div.ridetype").append('<label class="radio errorMessage">Please select an option</label>');
         }
     }
 
